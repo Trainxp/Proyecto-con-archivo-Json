194213742be3b23341c67ebd1ed7958e40781ee1
Nahuel Enriquez <61632891+Trainxp@users.noreply.github.com>
2022-05-04 20:00:52 -0300

    Subiendo archivos
    Subiendo archivos- cambio a Json

---
+++ stock.js
@@ -0,0 +1,23 @@
+
+
+
+let stockHardware = [
+
+    {id: 1, nombre: "Ryzen 9 5950x",  tipo: "procesador", cantidad: 1, desc: "procesador 16 nucleos", precio: 34000, marca: "AMD", img: './img/RENDERRYZEN95950X.jpg'},
+    {id: 2, nombre: "Ryzen 7 5800X",  tipo: "procesador", cantidad: 1, desc: "procesador 12 nucleos", precio: 61100, marca: "AMD", img: './img/RENDERRYZEN75800X.jpg'},
+    {id: 3, nombre: "Ryzen 5 5600x ", tipo: "procesador", cantidad: 1, desc: "procesador 6 nucleos", precio: 31200, marca: "AMD", img: './img/RENDERRYZEN55600X.jpg'},
+    {id: 4, nombre: "Ryzen 5 3600 ", tipo: "procesador", cantidad: 1, desc: "procesador 6 nucleos", precio: 31400, marca: "AMD", img: './img/RENDERRYZEN53600.jpg'},
+    {id: 5, nombre: "Intel i9 12900k", tipo: "procesador", cantidad: 1, desc: "procesador 12 nucleos", precio: 91200, marca: "INTEL", img: './img/RENDERINTELi912900K.jpg'},
+    {id: 6, nombre: "Intel i3 10200", tipo: "procesador", cantidad: 1, desc: "procesador 6 nucleos", precio: 21500, marca: "INTEL", img: './img/RENDERINTELi310200.jpg'},
+    {id: 7, nombre: "Intel i5 10400", tipo: "procesador", cantidad: 1, desc: "procesador 6 nucleos", precio: 51500, marca: "INTEL", img: './img/RENDERINTELi510400.jpg'},
+    {id: 8, nombre: "Intel i7 10700", tipo: "procesador", cantidad: 1, desc: "procesador 8 nucleos", precio: 41500, marca: "INTEL", img: './img/RENDERINTELi710700.jpg'},
+    {id: 9, nombre: "M Asus prime A320 M ", tipo: "mother", cantidad: 1, desc: "mother am4", precio: 8200, marca: "ASUS", img: './img/RENDERASUSPRIMEA320.jpg'},
+    {id: 10, nombre: "M Asrock A320M", tipo: "mother", cantidad: 1, desc: "Mother am4", precio: 7600, marca: "ASROCK", img: './img/RENDERASROCKA320.jpg'},
+    {id: 11, nombre: "M MSI MPG X570S EDGE", tipo: "mother", cantidad: 1, desc: "Mother am4", precio: 51500, marca: "MSI", img: './img/RENDERMSIMPG.jpg'},
+    {id: 12, nombre: "M Gigabyte B450 M", tipo: "mother", cantidad: 1, desc: "Mother am4", precio: 12200, marca: "GIGABYTE", img: './img/RENDERGIGABYTEB450H.jpg'},
+    {id: 13, nombre: "M MSI H310 M", tipo: "mother", cantidad: 1, desc: "Mother Intel 1151", precio: 8700, marca: "MSI", img: './img/RENDERMSIH310M.jpg'},
+    {id: 14, nombre: "M GIGABYTE Z590", tipo: "mother", cantidad: 1, desc: "Mother Intel 1200", precio: 33700, marca: "GIGABYTE", img: './img/RENDERGIGABUTEZ590.jpg'},
+    {id: 15, nombre: "M GIGABYTE Z590M", tipo: "mother", cantidad: 1, desc: "Mother Intel 1200", precio: 38200, marca: "GIGABYTE", img: './img/RENDERGIGABYTEZ590M.jpg'},
+    {id: 16, nombre: "PYN QUADRO 4000", tipo: "mother", cantidad: 1, desc: "Mother Intel 1200", precio: 15700, marca: "MSI", img: './img/RENDERMSIB560M.jpg'},
+
+]
